bc267647ed895b3e3528a285e665665ae70f025a
Alexander Filippov <a.filippov@yadro.com>
2020-09-14 18:15:16 +0300

    dbus: wait until the service is really stopped
    This adds a systemd signal handler and a waiting loop until the systemd
    unit is really stopped.
    
    Signed-off-by: Alexander Filippov <a.filippov@yadro.com>

--- src/dbus.cpp
+++ src/dbus.cpp
@@ -7,6 +7,8 @@
 
 #include "dbus.hpp"
 
+#include <sdbusplus/bus/match.hpp>
+
 sdbusplus::bus::bus systemBus = sdbusplus::bus::new_default();
 
 Objects getObjects(const Path& path, const Interfaces& ifaces)
@@ -51,7 +53,7 @@ void startUnit(const std::string& unitname)
     systemBus.call_noreply(req);
 }
 
-bool isUnitStarted(const std::string& unitname)
+bool doesUnitExist(const std::string& unitname)
 {
     auto req = systemBus.new_method_call(SYSTEMD_BUSNAME, SYSTEMD_PATH,
                                          SYSTEMD_INTERFACE, "GetUnit");
@@ -68,13 +70,61 @@ bool isUnitStarted(const std::string& unitname)
     return true;
 }
 
+void subscribeToSystemdSignals()
+{
+    auto req = systemBus.new_method_call(SYSTEMD_BUSNAME, SYSTEMD_PATH,
+                                         SYSTEMD_INTERFACE, "Subscribe");
+    systemBus.call_noreply(req);
+}
+
+void unsubscribeFromSystemdSignals()
+{
+    auto req = systemBus.new_method_call(SYSTEMD_BUSNAME, SYSTEMD_PATH,
+                                         SYSTEMD_INTERFACE, "Unsubscribe");
+    systemBus.call_noreply(req);
+}
+
 void stopUnit(const std::string& unitname)
 {
-    if (isUnitStarted(unitname))
+    if (doesUnitExist(unitname))
     {
+        bool serviceStopped = false;
+        sdbusplus::bus::match_t systemdSignals(
+            systemBus,
+            sdbusplus::bus::match::rules::type::signal() +
+                sdbusplus::bus::match::rules::member("JobRemoved") +
+                sdbusplus::bus::match::rules::path(SYSTEMD_PATH) +
+                sdbusplus::bus::match::rules::interface(SYSTEMD_INTERFACE),
+            [&](sdbusplus::message::message& msg) {
+                uint32_t newStateID;
+                sdbusplus::message::object_path newStateObjPath;
+                std::string newStateUnit;
+                std::string newStateResult;
+
+                msg.read(newStateID, newStateObjPath, newStateUnit,
+                         newStateResult);
+                if (newStateUnit == unitname && newStateResult == "done")
+                {
+                    serviceStopped = true;
+                }
+            });
+
+        subscribeToSystemdSignals();
+
         auto req = systemBus.new_method_call(SYSTEMD_BUSNAME, SYSTEMD_PATH,
                                              SYSTEMD_INTERFACE, "StopUnit");
         req.append(unitname, "replace");
         systemBus.call_noreply(req);
+
+        while (!serviceStopped)
+        {
+            systemBus.process_discard();
+            if (!serviceStopped)
+            {
+                systemBus.wait();
+            }
+        }
+
+        unsubscribeFromSystemdSignals();
     }
 }
